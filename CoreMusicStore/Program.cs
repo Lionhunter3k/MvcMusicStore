c02e3111cbe0d91596cbbff9fc9a1989bd677f1c
Cosmin Dodu <lionhunter@gmail.com>
2018-02-28 19:03:01 +0200

    added new mvc csproj

---
+++ CoreMusicStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreMusicStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
